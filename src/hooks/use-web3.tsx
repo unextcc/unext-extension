1e383cdad9bf6e97042092741a07281324d7fe4e
Fehat Ozkasgari <ferhat@dice.fm>
2023-02-21 22:27:04 -0500

    create-wallet

--- src/hooks/use-web3.tsx
+++ src/hooks/use-web3.tsx
@@ -1,5 +1,6 @@
 import Web3 from 'web3';
 import { useState } from "react";
+import { config } from "~contents/config";
 
 export const useWeb3CreateAccount = () => {
   const [account, setAccount] = useState<{ address: string; privateKey: string; encryptedPrivateKey: string }>({
@@ -8,6 +9,5 @@ export const useWeb3CreateAccount = () => {
   const [error, setError] = useState<{}>({});
   const [status, setStatus] = useState<string>("idle");
 
-  const createAccount = async (password: string, providerUrl: string) => {}
-
+  const createAccount = async (password: string, providerUrl: string = config.providerUrl) => {}
 }
