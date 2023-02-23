acfe11be1c464c37d283e8b68719a6b33e2e57e8
Fehat Ozkasgari <ferhat@dice.fm>
2023-02-23 11:55:37 -0500

    refactor config constant

--- src/contents/config.ts
+++ src/contents/config.ts
@@ -8,6 +8,10 @@ type envType = {
   etherScanUrl: string;
   tokens: {
     id: number;
+    alchemyApiKey: string;
+    alchemyNetwork: string;
+    alchemyMaxRetries: number;
+    blockchain: string;
     contractAddress: string;
     decimals: number;
     enabled: boolean;
@@ -16,29 +20,36 @@ type envType = {
     name: string;
     network: string;
     symbol: string;
+    scannerUrl: string;
+    providerUrl: string;
   }[];
 }
 
 const prod = <envType>({
-  providerUrl: "https://mainnet.infura.io/v3/b401a8aed4fd4473aa9442f1b4c5cf98",
-  alchemyApiKey: "4RXAc01ERD8TgwdOzJknjQSmU5fxCAKP",
-  alchemyNetwork: Network.ETH_MAINNET,
-  alchemyMaxRetries: 1,
-  etherScanUrl: 'https://etherscan.io/tx',
   tokens: [
     {
       id: 0,
-      contractAddress: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+      alchemyApiKey: 'vvihhyWLDnyBDx_UEdaB0f24ItIbeMyn',
+      alchemyNetwork: Network.MATIC_MAINNET,
+      alchemyMaxRetries: 1,
+      blockchain: 'polygon',
+      contractAddress: "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174",
       decimals: 6,
       enabled: true,
       fiat: 'USD',
       fiatSymbol: '$',
       name: 'USD Coin',
       network: "mainnet",
-      symbol: 'USDC'
+      symbol: 'USDC',
+      scannerUrl: 'https://polygonscan.com',
+      providerUrl: 'https://polygon-mainnet.infura.io/v3/b401a8aed4fd4473aa9442f1b4c5cf98'
     },
     {
       id: 1,
+      alchemyApiKey: '4RXAc01ERD8TgwdOzJknjQSmU5fxCAKP',
+      alchemyNetwork: Network.ETH_MAINNET,
+      alchemyMaxRetries: 1,
+      blockchain: 'ethereum',
       contractAddress: "0x1aBaEA1f7C830bD89Acc67eC4af516284b1bC33c",
       decimals: 6,
       enabled: true,
@@ -46,39 +57,48 @@ const prod = <envType>({
       fiatSymbol: '€',
       name: 'Euro Coin',
       network: "mainnet",
-      symbol: 'EUROC'
+      symbol: 'EUROC',
+      scannerUrl: 'https://etherscan.io',
+      providerUrl: 'https://mainnet.infura.io/v3/b401a8aed4fd4473aa9442f1b4c5cf98',
     }
   ],
 });
 
 const dev = <envType>({
-  providerUrl: "https://goerli.infura.io/v3/b401a8aed4fd4473aa9442f1b4c5cf98",
-  alchemyApiKey: "zFXR_i23EsYS1DJ13gb3IOXrf7iR82kR",
-  alchemyNetwork: Network.ETH_GOERLI,
-  alchemyMaxRetries: 1,
-  etherScanUrl: 'https://goerli.etherscan.io/tx',
   tokens: [
     {
       id: 0,
-      contractAddress: "0x07865c6E87B9F70255377e024ace6630C1Eaa37F",
+      alchemyApiKey: 'l1pSvMC4vAu8hap2qu5cN6nsuTnryRkT',
+      alchemyNetwork: Network.MATIC_MUMBAI,
+      alchemyMaxRetries: 1,
+      blockchain: 'polygon',
+      contractAddress: "0x992d00C09E1162Bda6D556A15d83e5050d300908",
       decimals: 6,
       enabled: true,
       fiat: 'USD',
       fiatSymbol: '$',
       name: 'USD Coin',
-      network: "goerli",
-      symbol: 'USDC'
+      network: "mumbai",
+      symbol: 'USDC',
+      scannerUrl: 'https://mumbai.polygonscan.com/',
+      providerUrl: 'https://polygon-mumbai.infura.io/v3/b401a8aed4fd4473aa9442f1b4c5cf98',
     },
     {
       id: 1,
+      alchemyApiKey: 'zFXR_i23EsYS1DJ13gb3IOXrf7iR82kR',
+      alchemyNetwork: Network.ETH_GOERLI,
+      alchemyMaxRetries: 1,
+      blockchain: 'ethereum',
       contractAddress: "0xA683d909e996052955500DDc45CA13E25c76e286",
       decimals: 6,
       enabled: true,
       fiat: 'EUR',
       fiatSymbol: '€',
       name: 'Euro Coin',
-      network: "goerli",
-      symbol: 'EUROC'
+      network: "mumbai",
+      symbol: 'EUROC',
+      scannerUrl: 'https://goerli.etherscan.io',
+      providerUrl: 'https://goerli.infura.io/v3/b401a8aed4fd4473aa9442f1b4c5cf98',
     }
   ]
 });
